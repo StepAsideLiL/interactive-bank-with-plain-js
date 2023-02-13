22c76095b001c09f6d50761cef85a0165b539287
stepasidelil <rifat6633@gmail.com>
2023-02-13 15:58:47 +0600

    bacis deposit and withdraw functionality is added

--- deposit.js
+++ deposit.js
@@ -1,3 +1,51 @@
 (function() {
+  // Selecting the necessary DOM.
+  const totalDeposit = document.querySelector('#total-deposit');
+  const totalWithdraw = document.querySelector('#total-withdraw');
+  const totalBalance = document.querySelector('#total-balance');
+
+  const depositInputField = document.querySelector('#deposit-field');
+  const depositSubmitBtn = document.querySelector('#deposit-button');
+  const withdrawInputField = document.querySelector('#withdraw-field');
+  const withdrawSubmitBtn = document.querySelector('#withdraw-button');
+
+  // Defining necessary global variable.
+  let currentDeposit = parseInt(totalDeposit.innerText);
+  let currentWithdraw = parseInt(totalWithdraw.innerText);
+  let currentBalance = parseInt(totalBalance.innerText);
+  let newDepositAmount;
+  let newWithdrawAmount;
   
+  // console.log(deposit + withdraw + balance);
+
+  /**
+   * Adding event listener to enable the deposit button.
+   */
+  depositInputField.addEventListener('keyup', function(e) {
+    depositSubmitBtn.disabled = e.target.value.length >= 1 ? false : true;
+  });
+
+  /**
+   * Adding event listener to enable the withdraw button.
+   */
+  withdrawInputField.addEventListener('keyup', function(e) {
+    withdrawSubmitBtn.disabled = e.target.value.length >= 1 ? false : true;
+  });
+
+  /**
+   * Adding event listener to deposit money.
+   */
+  depositSubmitBtn.addEventListener('click', function() {
+    totalDeposit.innerText = parseInt(depositInputField.value) + currentDeposit;
+    totalBalance.innerText = parseInt(depositInputField.value) + currentBalance;
+  });
+
+  /**
+   * Adding event listener to withdraw money.
+   */
+  withdrawSubmitBtn.addEventListener('click', function() {
+    totalWithdraw.innerText = currentDeposit - parseInt(withdrawInputField.value);
+    totalBalance.innerText = currentBalance - parseInt(withdrawInputField.value);
+  });
+
 })()
